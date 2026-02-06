cb15773b9de2d9b3e824f3b6dab9ac65fc491b67
c.bringer <corentin.bringer@fideciel.fr>
2026-02-06 15:56:41 +0100

    gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,27 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			head: [
+				// Charger le script gtag.js
+				{
+					tag: 'script',
+					attrs: {
+						async: true,
+						src: 'https://www.googletagmanager.com/gtag/js?id=G-4MDJ9HQJVN'
+					},
+				},
+				// Initialiser Google Analytics
+				{
+					tag: 'script',
+					content: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-4MDJ9HQJVN');
+					`,
+				},
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Pitchouneee' }],
 			sidebar: [
 				{
 					label: 'Guides',
